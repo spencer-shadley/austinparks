41c0f7ef38fdc2feab95580873aa8ac60e33886f
Spencer Shadley <spencershadley@utexas.edu>
2016-04-14 20:57:53 -0500

    added angular data automatic loading

--- js/generic.js
+++ js/generic.js
@@ -1,26 +1,42 @@
 console.log('generic.js');
 
+var currentPoolJson;
+
 var app = angular.module('app', []);
 app.controller('ctrl', function($scope, $http) {
     $http({
         method : "GET",
         url : "poolnames"
     }).then(function(res) {
-        res.data.forEach(function(pool) {
+        res.data.forEach(function(poolName) {
             $('#poolNameList').append(
-                $('<li id="' + pool + '">').append(
+                $('<li id="' + poolName + '">').append(
                     $('<button class="btn btn-link">').append(
-                        pool
+                        poolName
                     ).click(function() {
-                        console.log(pool);
+                        setPool(poolName);
                     })
                 )
             );
         });
     });
 
+
+
     $scope.map = "asdflsdkfjsdlkfj";
     console.log('got here');
+
+    function setPool(poolName) {
+        console.log('set pool' + poolName);
+        $http({
+            method: "GET",
+            url: "pooldata/" + poolName
+        }).then(function(res) {
+            currentPoolJson = res.data;
+            $scope.poolJson = res.data;
+            console.log(currentPoolJson);
+        });
+    }
 
 });
 
