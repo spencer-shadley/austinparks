28b3906e0698e221dcfe7942d1ea8be2399ea2f5
spencer-shadley <spencershadley@utexas.edu>
2016-04-19 17:50:34 -0500

    added ability to click on a pool and drop a marker

--- js/generic.js
+++ js/generic.js
@@ -1,6 +1,5 @@
-console.log('generic.js');
-
 var currentPoolJson;
+var map;
 
 var app = angular.module('app', []);
 app.controller('ctrl', function($scope, $http) {
@@ -27,17 +26,21 @@ app.controller('ctrl', function($scope, $http) {
             method: "GET",
             url: "pooldata/" + poolName
         }).then(function(res) {
+
+            // save data for later reference
             currentPoolJson = res.data;
             $scope.poolJson = res.data;
-            console.log(currentPoolJson);
+
+            // create marker for pool
+            createMarker(currentPoolJson.location_1.latitude, currentPoolJson.location_1.longitude, currentPoolJson.pool_name);
+
+            // format opening/closing hours based on availability
             var open_div = document.getElementById("status_open");
             var closed_div = document.getElementById("status_closed");
             if (currentPoolJson.status === "Closed") {
                 closed_div.classList.remove("hidden");
                 open_div.classList.add("hidden");
-                // Need to reformat date to a readable string
-                var d = new Date(currentPoolJson.open_date).toDateString();
-                currentPoolJson.open_date = d;
+                currentPoolJson.open_date = new Date(currentPoolJson.open_date).toDateString();
             } else {
                 open_div.classList.remove("hidden");
                 closed_div.classList.add("hidden");
@@ -52,23 +55,25 @@ app.controller('ctrl', function($scope, $http) {
             zoom: 10,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
-        var map = new google.maps.Map(document.getElementById("map"), mapProp);
+        map = new google.maps.Map(document.getElementById("map"), mapProp);
+    }
+    google.maps.event.addDomListener(window, 'load', initMap);
 
+    function createMarker(latitude, longitude, infoData) {
         // create markers (pins)
         var marker = new google.maps.Marker({
-            position: {lat: 30.3079827, lng: -97.8934848},
+            position: {lat: Number(latitude), lng: Number(longitude)},
             animation: google.maps.Animation.DROP
         });
         marker.setMap(map);
 
         // create info windows
         var infoWindow = new google.maps.InfoWindow({
-           content: 'hello world'
+            content: infoData
         });
         marker.addListener('click', function() {
             infoWindow.open(map, marker);
         });
     }
-    google.maps.event.addDomListener(window, 'load', initMap);
 });
 
